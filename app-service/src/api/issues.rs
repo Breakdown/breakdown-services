bd9ccfa7486c53bc425c2c6760b319c6504389ee
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2022-11-22 18:24:35 -0800

    Add follow/unfollow routes

--- app-service/src/api/issues.rs
+++ app-service/src/api/issues.rs
@@ -5,7 +5,7 @@ use axum::{
 };
 
 use crate::{
-    services::issues::{follow_issue, get_issue_by_id, get_issues},
+    services::issues::{follow_issue, get_issue_by_id, get_issues, unfollow_issue},
     utils::auth::create_session_auth_layer,
 };
 
@@ -14,6 +14,7 @@ pub fn router() -> Router {
         .route("/", get(get_issues))
         .route("/:id", get(get_issue_by_id))
         .route("/:id/follow", post(follow_issue))
+        .route("/:id/unfollow", post(unfollow_issue))
         .route_layer(middleware::from_fn(create_session_auth_layer));
     Router::new().nest("/issues", service_router)
 }
--- app-service/src/services/issues.rs
+++ app-service/src/services/issues.rs
@@ -51,12 +51,65 @@ pub async fn follow_issue(
     ctx: Extension<ApiContext>,
     Path(params): Path<HashMap<String, String>>,
     session: ReadableSession,
-) -> Result<Json<ResponseBody<BreakdownIssue>>, ApiError> {
+) -> Result<Json<ResponseBody<String>>, ApiError> {
     let issue_id = match Uuid::parse_str(&params.get("id").unwrap().to_string()) {
         Ok(issue_id) => issue_id,
         Err(_) => return Err(ApiError::NotFound),
     };
-    println!("user_id: {:?}", session.get::<Uuid>("user_id").unwrap());
-    // TODO: Follow issue
-    todo!();
+    let user_id = session.get::<Uuid>("user_id").unwrap();
+    let existing_relation = sqlx::query!(
+        r#"
+          SELECT * FROM users_issues WHERE issue_id = $1 AND user_id = $2
+        "#,
+        issue_id,
+        user_id
+    )
+    .fetch_optional(&ctx.connection_pool)
+    .await?;
+
+    match existing_relation {
+        Some(_) => {
+            return Ok(Json(ResponseBody {
+                data: "ok".to_string(),
+            }))
+        }
+        None => {
+            sqlx::query!(
+                r#"
+                  INSERT INTO users_issues (issue_id, user_id) VALUES ($1, $2)
+                "#,
+                issue_id,
+                user_id
+            )
+            .execute(&ctx.connection_pool)
+            .await?;
+            Ok(Json(ResponseBody {
+                data: "ok".to_string(),
+            }))
+        }
+    }
+}
+
+pub async fn unfollow_issue(
+    ctx: Extension<ApiContext>,
+    Path(params): Path<HashMap<String, String>>,
+    session: ReadableSession,
+) -> Result<Json<ResponseBody<String>>, ApiError> {
+    let issue_id = match Uuid::parse_str(&params.get("id").unwrap().to_string()) {
+        Ok(issue_id) => issue_id,
+        Err(_) => return Err(ApiError::NotFound),
+    };
+    let user_id = session.get::<Uuid>("user_id").unwrap();
+    sqlx::query!(
+        r#"
+          DELETE FROM users_issues WHERE issue_id = $1 AND user_id = $2
+        "#,
+        issue_id,
+        user_id
+    )
+    .execute(&ctx.connection_pool)
+    .await?;
+    Ok(Json(ResponseBody {
+        data: "ok".to_string(),
+    }))
 }
