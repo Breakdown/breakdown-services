fd5f5ecab9f9b62b7c27c4c161ef7585128f7638
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2024-01-31 14:46:09 -0800

    GET bill by ID

--- app-service/src/bills/BillsService.ts
+++ app-service/src/bills/BillsService.ts
@@ -1,5 +1,16 @@
+import dbClient from "../utils/prisma.js";
+
 class BillsService {
   constructor() {}
+
+  async getBillById(id: string) {
+    const dbResponse = await dbClient.bill.findUnique({
+      where: {
+        id,
+      },
+    });
+    return dbResponse;
+  }
 }
 
 export default BillsService;
--- app-service/src/bills/router.ts
+++ app-service/src/bills/router.ts
@@ -1,5 +1,28 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { param } from "express-validator";
+import {
+  errorPassthrough,
+  handleValidationErrors,
+  requireAuth,
+} from "../utils/express.js";
+import BillsService from "./BillsService.js";
 
 const router = Router();
+
+router.get(
+  "/:id",
+  [param("id").exists()],
+  errorPassthrough(handleValidationErrors),
+  errorPassthrough(requireAuth),
+  errorPassthrough(async (req: Request, res: Response) => {
+    const billsService = new BillsService();
+    const bill = await billsService.getBillById(req.params.id);
+    res.status(201).send({
+      data: {
+        bill,
+      },
+    });
+  })
+);
 
 export default router;
