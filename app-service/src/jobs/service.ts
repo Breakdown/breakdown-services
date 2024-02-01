7aeec95109def3f145b0cbf8858fb996e9d7fa72
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2024-02-01 12:37:15 -0800

    Reps sync finished, bills almost there

--- app-service/src/jobs/service.ts
+++ app-service/src/jobs/service.ts
@@ -1,6 +1,8 @@
 import { Queue, ConnectionOptions, Worker } from "bullmq";
 import dbClient from "../utils/prisma.js";
-import { Bill } from "@prisma/client";
+import { Bill, Prisma, Representative } from "@prisma/client";
+import PropublicaService from "../propublica/service.js";
+import { ProPublicaBill, PropublicaMember } from "../propublica/types.js";
 
 class JobService {
   queue: Queue;
@@ -18,13 +20,21 @@ class JobService {
   async queueRepsSyncScheduled() {
     // Add repsSync job to queue
     // Repeat every 12 hours at 03:00 and 15:00
-    this.queue.add("reps-sync", {}, { repeat: { pattern: "0 3,15 * * *" } });
+    this.queue.add(
+      "reps-sync-scheduled",
+      {},
+      { repeat: { pattern: "0 3,15 * * *" } }
+    );
   }
 
   async queueBillsSyncScheduled() {
     // Add billsSync job to queue
     // Repeat every 12 hours at 06:00 and 18:00
-    this.queue.add("bills-sync", {}, { repeat: { pattern: "0 6,18 * * *" } });
+    this.queue.add(
+      "bills-sync-scheduled",
+      {},
+      { repeat: { pattern: "0 6,18 * * *" } }
+    );
   }
 
   async queueBillFullTextsScheduled() {
@@ -106,13 +116,177 @@ class JobService {
     // TODO: Meilisearch connection and syncing here
   }
 
+  transformPropublicaMemberToDbRep(
+    member: PropublicaMember
+  ): Partial<Representative> {
+    const houseValue = member.short_title === "Sen." ? "senate" : "house";
+    return {
+      title: member.title,
+      shortTitle: member.short_title,
+      apiUri: member.api_uri,
+      firstName: member.first_name,
+      middleName: member.middle_name,
+      lastName: member.last_name,
+      suffix: member.suffix,
+      dateOfBirth: member.date_of_birth
+        ? new Date(member.date_of_birth)
+        : undefined,
+      gender: member.gender,
+      party: member.party,
+      propublicaId: member.id,
+      twitter: member.twitter_account,
+      facebook: member.facebook_account,
+      govtrackId: member.id,
+      cspanId: member.cspan_id,
+      votesmartId: member.votesmart_id,
+      icpsrId: member.icpsr_id,
+      crpId: member.crp_id,
+      googleEntityId: member.google_entity_id,
+      fecCandidateId: member.fec_candidate_id,
+      url: member.url,
+      rssUrl: member.rss_url,
+      contactForm: member.contact_form,
+      inOffice: member.in_office,
+      cookPvi: member.cook_pvi, // Not filling - not in the API response? Or mismapped?
+      dwNominate: member.dw_nominate,
+      seniority: member.seniority,
+      nextElection: member.next_election
+        ? new Date(member.next_election)
+        : undefined,
+      totalVotes: member.total_votes,
+      missedVotes: member.missed_votes,
+      totalPresent: member.total_present,
+      lastUpdated: member.last_updated
+        ? new Date(member.last_updated)
+        : undefined,
+      ocdId: member.ocd_id,
+      office: member.office,
+      phone: member.phone,
+      fax: member.fax,
+      state: member.state,
+      district: member.district,
+      senateClass: member.senate_class,
+      stateRank: member.state_rank,
+      lisId: member.lis_id,
+      missedVotesPct: member.missed_votes_pct,
+      votesWithPartyPct: member.votes_with_party_pct,
+      votesAgainstPartyPct: member.votes_against_party_pct,
+      house: houseValue,
+    } as Partial<Representative>;
+  }
+
+  transformPropublicaBillToDbBill(bill: ProPublicaBill): Bill {
+    return {
+      propublicaId: bill.bill_id,
+    } as Bill;
+  }
+
+  async scheduledRepsSync() {
+    // Fetch all representatives from the house and senate
+    // Upsert all of them matching on propublica ID
+    const propubService = new PropublicaService();
+    const houseMembers = await propubService.fetchMembers({
+      chamber: "house",
+      offset: 0,
+    });
+    const senateMembers = await propubService.fetchMembers({
+      chamber: "senate",
+      offset: 0,
+    });
+    const allMembers = [...houseMembers, ...senateMembers];
+    allMembers.map((member) => {
+      if (member.cook_pvi) {
+        console.log("cook pvi", member.cook_pvi);
+      } else {
+        console.log("no cook pvi");
+      }
+    });
+    await dbClient.$transaction(
+      allMembers.map((propubMember) =>
+        dbClient.representative.upsert({
+          where: {
+            propublicaId: propubMember.id,
+          },
+          update: this.transformPropublicaMemberToDbRep(propubMember),
+          create: this.transformPropublicaMemberToDbRep(propubMember),
+        })
+      )
+    );
+  }
+
+  async scheduledBillsSync() {
+    // Fetch last 40 bills from the API
+    const numBillsFetched = 60; // Keep in increments of 20
+    const propubService = new PropublicaService();
+    // Loop through chunks in incrememts of 20 and create fetch requests
+    let introducedFetches = [];
+    let updatedFetches = [];
+    for (let i = 0; i < numBillsFetched; i += 20) {
+      // If last chunk of size < 20, queue remaining at offset
+      if (i >= numBillsFetched) {
+        break;
+      }
+      // Fetch next 20 introduced
+      introducedFetches.push(
+        propubService.fetchBills({
+          type: "introduced",
+          offset: i,
+        })
+      );
+      // Fetch next 20 updated
+      updatedFetches.push(
+        propubService.fetchBills({
+          type: "updated",
+          offset: i,
+        })
+      );
+    }
+    // Fetch all bills
+    const introducedBills = await Promise.all(introducedFetches);
+    const updatedBills = await Promise.all(updatedFetches);
+    // Flatten arrays
+    const allIntroducedBills = introducedBills.flat();
+    const allUpdatedBills = updatedBills.flat();
+    // Prioritize updates over introductions - OoO matters - if dupe updated will have most up to date data
+    const allBills = [...allUpdatedBills, ...allIntroducedBills];
+    let idToSeenMap: { [key: string]: boolean } = {};
+    // Dedupe on bill.bill_id
+    const allBillsDeduped = allBills.filter((bill) => {
+      if (idToSeenMap[bill.bill_id]) {
+        return false;
+      } else {
+        idToSeenMap[bill.bill_id] = true;
+        return true;
+      }
+    });
+    // TODO: Get sponsor DB ID for introduced bills
+    const introducedBillPropubIdToSponsorDbIdMap: { [key: string]: string } =
+      {};
+    for (const bill of allIntroducedBills) {
+      const sponsor = await dbClient.representative.findUnique({
+        where: {
+          propublicaId: bill.sponsor_id,
+        },
+      });
+      if (sponsor) {
+        introducedBillPropubIdToSponsorDbIdMap[bill.bill_id] = sponsor.id;
+      }
+    }
+    // TODO: Get primary issue for bill
+
+    // TODO: Trigger cosponsors sync job for this bill
+    // TODO: Trigger votes sync job for this bill if it has been voted on (and set lastVotesSync on bill job data)
+  }
+
   async createWorkers() {
     // Create workers
     // repsSync worker
     const repsWorker = new Worker(
       "app-service-queue",
       async (job) => {
-        console.log("reps-sync job started");
+        if (job.name === "reps-sync-scheduled") {
+          await this.scheduledRepsSync();
+        }
       },
       {
         connection: this.redisConnection,
---
+++ app-service/src/propublica/types.ts
@@ -0,0 +1,130 @@
+interface ProPublicaResponse<T> {
+  results: T[];
+}
+
+interface PropublicaMembersResult {
+  members: PropublicaMember[];
+}
+
+interface PropublicaBillsResult {
+  bills: ProPublicaBill[];
+}
+
+interface CosponsorsByParty {
+  D: number;
+  R: number;
+}
+export interface ProPublicaBill {
+  bill_id: string;
+  bill_slug?: string;
+  bill_type?: string;
+  number?: string;
+  bill_uri?: string;
+  title?: string;
+  short_title?: string;
+  sponsor_title?: string;
+  sponsor_id?: string;
+  sponsor_name?: string;
+  sponsor_state?: string;
+  sponsor_party?: string;
+  sponsor_uri?: string;
+  gpo_pdf_uri?: string;
+  congressdotgov_url?: string;
+  govtrack_url?: string;
+  introduced_date?: string;
+  active?: boolean;
+  last_vote?: string;
+  house_passage?: string;
+  senate_passage?: string;
+  enacted?: string;
+  vetoed?: string;
+  cosponsors?: number;
+  cosponsors_by_party?: CosponsorsByParty;
+  committees?: string;
+  committee_codes?: string[];
+  subcommittee_codes?: string[];
+  primary_subject?: string;
+  summary?: string;
+  summary_short?: string;
+  latest_major_action_date?: string;
+  latest_major_action?: string;
+}
+
+export interface PropublicaMember {
+  id: string;
+  title: string;
+  short_title: string;
+  api_uri: string;
+  first_name: string;
+  middle_name?: string;
+  last_name: string;
+  suffix?: string;
+  date_of_birth: string;
+  gender: string;
+  party: string;
+  leadership_role?: string;
+  twitter_account?: string;
+  facebook_account?: string;
+  youtube_account?: string;
+  govtrack_id?: string;
+  cspan_id?: string;
+  votesmart_id?: string;
+  icpsr_id?: string;
+  crp_id?: string;
+  google_entity_id?: string;
+  fec_candidate_id?: string;
+  url?: string;
+  rss_url?: string;
+  contact_form?: string;
+  in_office?: boolean;
+  cook_pvi?: string;
+  dw_nominate?: number;
+  seniority?: string;
+  next_election?: string;
+  total_votes?: number;
+  missed_votes?: number;
+  total_present?: number;
+  last_updated?: string;
+  ocd_id?: string;
+  office?: string;
+  phone?: string;
+  fax?: string;
+  state?: string;
+  district?: string;
+  senate_class?: string;
+  state_rank?: string;
+  lis_id?: string;
+  missed_votes_pct?: number;
+  votes_with_party_pct?: number;
+  votes_against_party_pct?: number;
+}
+
+interface ProPublicaVoteBill {
+  bill_id?: string;
+  number?: string;
+  sponsor_id?: string;
+  bill_uri?: string;
+  title?: string;
+  latest_action?: string;
+}
+
+export interface ProPublicaVote {
+  member_id?: string;
+  chamber?: string;
+  congress?: string;
+  session?: string;
+  roll_call?: string;
+  vote_uri?: string;
+  bill?: ProPublicaVoteBill;
+  question?: string;
+  result?: string;
+  date?: string;
+  time?: string;
+  position?: string;
+}
+
+export interface PropublicaBillsResponse
+  extends ProPublicaResponse<PropublicaBillsResult> {}
+
+export interface PropublicaMembersResponse
+  extends ProPublicaResponse<PropublicaMembersResult> {}
