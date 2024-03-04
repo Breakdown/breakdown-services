3a88b97aae7afa074eab876b68cd67b9052519ee
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2024-03-03 17:49:26 -0800

    Cache keys created

--- app-service/src/cache/service.ts
+++ app-service/src/cache/service.ts
@@ -1,6 +1,16 @@
 import { Redis } from "ioredis";
 import { redis } from "../utils/redis.js";
 
+enum CacheDataKeys {
+  BILLS_FOR_USER,
+  USERS_INTERESTED_IN_BILL,
+  PROPUBLICA_FETCH_BILLS,
+  PROPUBLICA_SUBJECTS_FOR_BILL,
+  PROPUBLICA_FETCH_COSPONSORS_FOR_BILL,
+  PROPUBLICA_FETCH_VOTES_FOR_BILL,
+  PROPUBLICA_FETCH_MEMBERS,
+  PROPUBLICA_FETCH_REP_VOTES_FOR_BILL,
+}
 class CacheService {
   redis: Redis;
 
@@ -15,6 +25,38 @@ class CacheService {
   public async getJson<T>(key: string): Promise<T | undefined> {
     const value = await this.redis.get(key);
     return value ? (JSON.parse(value) as T) : undefined;
+  }
+
+  public async setData(key: CacheDataKeys) {}
+
+  public async bustCache(
+    key: CacheDataKeys,
+    {
+      userId,
+      billId,
+      propublicaUrl,
+    }: {
+      billId?: string;
+      userId?: string;
+      propublicaUrl?: string;
+    } = {}
+  ): Promise<void> {
+    switch (key) {
+      case CacheDataKeys.BILLS_FOR_USER:
+        if (userId) {
+          await this.delete(`${CacheDataKeys.BILLS_FOR_USER}:${userId}`);
+        }
+        break;
+      case CacheDataKeys.USERS_INTERESTED_IN_BILL:
+        if (billId) {
+          await this.delete(
+            `${CacheDataKeys.USERS_INTERESTED_IN_BILL}:${billId}`
+          );
+        }
+        break;
+      default:
+        throw new Error("Invalid cache key");
+    }
   }
 
   public set(
