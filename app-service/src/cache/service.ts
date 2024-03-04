2f2846dcddf8bdd6347ebdcb905832532531c56c
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2024-03-03 18:52:25 -0800

    Caching implemented in propublica service

--- app-service/src/cache/service.ts
+++ app-service/src/cache/service.ts
@@ -1,24 +1,25 @@
+import bcrypt from "bcryptjs";
 import { Redis } from "ioredis";
 import { redis } from "../utils/redis.js";
 import InternalError from "../utils/errors/InternalError.js";
 
-enum CacheDataKeys {
+export enum CacheDataKeys {
   PROPUBLICA_FETCH_BILLS,
   PROPUBLICA_SUBJECTS_FOR_BILL,
   PROPUBLICA_FETCH_COSPONSORS_FOR_BILL,
   PROPUBLICA_FETCH_VOTES_FOR_BILL,
   PROPUBLICA_FETCH_MEMBERS,
-  PROPUBLICA_FETCH_REP_VOTES_FOR_BILL,
+  PROPUBLICA_FETCH_REP_VOTES_FOR_BILL_VOTE,
   // User-related
   BILLS_FOR_USER,
-  USERS_INTERESTED_IN_BILL,
   LOCAL_REPS,
   // Non-user-related
+  USERS_INTERESTED_IN_BILL,
   REP_STATS_BY_ID,
   REP_VOTES_BY_ID,
   SPONSORED_BILLS_BY_REP_ID,
   COSPONSORED_BILLS_BY_REP_ID,
-  REPS_BY_DISTRICT_AND_STATE,
+  REPS_BY_STATE_AND_DISTRICT,
   REP_VOTE_ON_BILL,
   FEATURED_REPS,
 }
@@ -28,6 +29,8 @@ interface CacheKeyData {
   userId?: string;
   propublicaUrl?: string;
   representativeId?: string;
+  district?: string;
+  state?: string;
 }
 class CacheService {
   redis: Redis;
@@ -45,46 +48,62 @@ class CacheService {
     return value ? (JSON.parse(value) as T) : undefined;
   }
 
-  private createCacheKeyFromData(
+  private async hashKey(key: string): Promise<string> {
+    const hashedKey = await bcrypt.hash(key, 10);
+    return hashedKey;
+  }
+
+  private async createCacheKeyFromKeyAndData(
     key: CacheDataKeys,
-    { userId, billId, propublicaUrl, representativeId }: CacheKeyData
-  ): string {
+    {
+      userId,
+      billId,
+      propublicaUrl,
+      representativeId,
+      district,
+      state,
+    }: CacheKeyData
+  ): Promise<string> {
     switch (key) {
       // ProPublica-related
       case CacheDataKeys.PROPUBLICA_FETCH_BILLS:
-        return `propublica_fetch_bills:${propublicaUrl}`;
+        return this.hashKey(`propublica_fetch_bills:${propublicaUrl}`);
       case CacheDataKeys.PROPUBLICA_SUBJECTS_FOR_BILL:
-        return `propublica_subjects_for_bill:${propublicaUrl}`;
+        return this.hashKey(`propublica_subjects_for_bill:${propublicaUrl}`);
       case CacheDataKeys.PROPUBLICA_FETCH_COSPONSORS_FOR_BILL:
-        return `propublica_fetch_cosponsors_for_bill:${propublicaUrl}`;
+        return this.hashKey(
+          `propublica_fetch_cosponsors_for_bill:${propublicaUrl}`
+        );
       case CacheDataKeys.PROPUBLICA_FETCH_VOTES_FOR_BILL:
-        return `propublica_fetch_votes_for_bill:${propublicaUrl}`;
+        return this.hashKey(`propublica_fetch_votes_for_bill:${propublicaUrl}`);
       case CacheDataKeys.PROPUBLICA_FETCH_MEMBERS:
-        return `propublica_fetch_members:${propublicaUrl}`;
-      case CacheDataKeys.PROPUBLICA_FETCH_REP_VOTES_FOR_BILL:
-        return `propublica_fetch_rep_votes_for_bill:${propublicaUrl}`;
+        return this.hashKey(`propublica_fetch_members:${propublicaUrl}`);
+      case CacheDataKeys.PROPUBLICA_FETCH_REP_VOTES_FOR_BILL_VOTE:
+        return this.hashKey(
+          `propublica_fetch_rep_votes_for_bill_vote:${propublicaUrl}`
+        );
       // User-related
       case CacheDataKeys.BILLS_FOR_USER:
-        return `bills_for_user:${userId}`;
-      case CacheDataKeys.USERS_INTERESTED_IN_BILL:
-        return `users_interested_in_bill:${billId}`;
+        return this.hashKey(`bills_for_user:${userId}`);
       case CacheDataKeys.LOCAL_REPS:
-        return `local_reps:${userId}`;
+        return this.hashKey(`local_reps:${userId}`);
       // Non-user-related
+      case CacheDataKeys.USERS_INTERESTED_IN_BILL:
+        return this.hashKey(`users_interested_in_bill:${billId}`);
       case CacheDataKeys.REP_STATS_BY_ID:
-        return `rep_stats_by_id:${representativeId}`;
+        return this.hashKey(`rep_stats_by_id:${representativeId}`);
       case CacheDataKeys.REP_VOTES_BY_ID:
-        return `rep_votes_by_id:${representativeId}`;
+        return this.hashKey(`rep_votes_by_id:${representativeId}`);
       case CacheDataKeys.SPONSORED_BILLS_BY_REP_ID:
-        return `sponsored_bills_by_rep_id:${userId}`;
+        return this.hashKey(`sponsored_bills_by_rep_id:${representativeId}`);
       case CacheDataKeys.COSPONSORED_BILLS_BY_REP_ID:
-        return `cosponsored_bills_by_rep_id:${userId}`;
-      case CacheDataKeys.REPS_BY_DISTRICT_AND_STATE:
-        return `reps_by_district_and_state`;
+        return this.hashKey(`cosponsored_bills_by_rep_id:${representativeId}`);
+      case CacheDataKeys.REPS_BY_STATE_AND_DISTRICT:
+        return this.hashKey(`reps_by_state_and_district:${state}:${district}`);
       case CacheDataKeys.REP_VOTE_ON_BILL:
-        return `rep_vote_on_bill:${billId}`;
+        return this.hashKey(`rep_vote_on_bill:${representativeId}:${billId}`);
       case CacheDataKeys.FEATURED_REPS:
-        return `featured_reps`;
+        return this.hashKey(`featured_reps`);
       default:
         throw new Error("Invalid cache key");
     }
@@ -103,16 +122,16 @@ class CacheService {
         return 3600;
       case CacheDataKeys.PROPUBLICA_FETCH_MEMBERS:
         return 3600;
-      case CacheDataKeys.PROPUBLICA_FETCH_REP_VOTES_FOR_BILL:
+      case CacheDataKeys.PROPUBLICA_FETCH_REP_VOTES_FOR_BILL_VOTE:
         return 3600;
       // User-related
       case CacheDataKeys.BILLS_FOR_USER:
-        return 3600;
+        return 60 * 60 * 24; // 1 day
+      case CacheDataKeys.LOCAL_REPS:
+        return 60 * 60 * 24 * 7; // 1 week
+      // Non-user-related
       case CacheDataKeys.USERS_INTERESTED_IN_BILL:
         return 3600;
-      case CacheDataKeys.LOCAL_REPS:
-        return 3600;
-      // Non-user-related
       case CacheDataKeys.REP_STATS_BY_ID:
         return 3600;
       case CacheDataKeys.REP_VOTES_BY_ID:
@@ -121,7 +140,7 @@ class CacheService {
         return 3600;
       case CacheDataKeys.COSPONSORED_BILLS_BY_REP_ID:
         return 3600;
-      case CacheDataKeys.REPS_BY_DISTRICT_AND_STATE:
+      case CacheDataKeys.REPS_BY_STATE_AND_DISTRICT:
         return 3600;
       case CacheDataKeys.REP_VOTE_ON_BILL:
         return 3600;
@@ -132,12 +151,21 @@ class CacheService {
     }
   }
 
+  public async getData<T>(
+    key: CacheDataKeys,
+    keyData: CacheKeyData = {}
+  ): Promise<T | undefined> {
+    const cacheKey = await this.createCacheKeyFromKeyAndData(key, keyData);
+    const value = await this.getJson<T>(cacheKey);
+    return value;
+  }
+
   public async setData(
     key: CacheDataKeys,
     data: any,
     keyData: CacheKeyData = {}
   ): Promise<boolean> {
-    const cacheKey = this.createCacheKeyFromData(key, keyData);
+    const cacheKey = await this.createCacheKeyFromKeyAndData(key, keyData);
     const expirationTime = this.getExpirationTimeFromKey(key);
     await this.set(cacheKey, JSON.stringify(data), expirationTime);
     return true;
@@ -147,7 +175,7 @@ class CacheService {
     key: CacheDataKeys,
     data: CacheKeyData = {}
   ): Promise<boolean> {
-    const cacheKey = this.createCacheKeyFromData(key, data);
+    const cacheKey = await this.createCacheKeyFromKeyAndData(key, data);
     await this.delete(cacheKey);
     return true;
   }
